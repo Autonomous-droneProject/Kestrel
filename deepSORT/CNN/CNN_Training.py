5441154be8ce704b037704ba8b6a5c7d554ce8ee
Fernando <crespofernando05@gmail.com>
2025-06-24 19:46:22 -0400

    create data loader file

--- deepSORT/CNN/CNN_Training.py
+++
@@ -1,7 +0,0 @@
-import torch
-from model import CNNdeepSORT
-
-model = CNNdeepSORT(128, 128)
-fake_tensor = torch.randn(4, 3, 200, 200)
-output = model(fake_tensor)
-print(output.shape)
---
+++ deepSORT/CNN/Data_Loader.py
@@ -0,0 +1,27 @@
+import os
+import torch
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+
+class Market1501(Dataset):
+    def __init__(self, dir):
+        self.dir = dir
+        self.paths = [ file for file in os.listdir(dir) if file.endswith(".jpg") ]
+        self.ids = set([ path.split("_")[0] for path in self.paths ])
+        self.id_to_label = {pid: val for val, pid in enumerate(self.ids)}
+        self.num_classes = len(self.id_to_label)
+
+    def __getitem__(self, index):
+        # Get name and path corresponding to index
+        name = self.paths[index]
+        path = os.path.join(self.dir, name)
+
+        image = Image.open(path).convert("RGB")
+        pid = name.split("_")[0] # gets id from image
+        label = self.id_to_label[pid] # converts string id to clean int label
+
+        return image, torch.tensor(label, dtype=torch.long)
+    
+    def __len__(self):
+        return len(self.paths)
+            
