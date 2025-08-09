066e2e7dcc0b5b3b4d42e02cbfeae0bf4cca9b6d
BigManRaffa <animedudebro@gmail.com>
2025-08-08 20:35:57 -0400

    ardupilot translator stuff (not c word) [am i learning?]

---
+++ src/kestrel_control/include/kestrel_control/ardupilot_translator_node.hpp
@@ -0,0 +1,28 @@
+#ifndef KESTREL_CONTROL__ARDUPILOT_TRANSLATOR_NODE_HPP_
+#define KESTREL_CONTROL__ARDUPILOT_TRANSLATOR_NODE_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "mavros_msgs/msg/position_target.hpp"
+
+namespace kestrel_control
+{
+
+class ArduPilotTranslatorNode : public rclcpp::Node
+{
+public:
+  explicit ArduPilotTranslatorNode(const rclcpp::NodeOptions & options);
+
+private:
+  void initialize();
+  void waypoint_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
+
+  // subscribes to high-level waypoints from the path planner
+  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr waypoint_sub_;
+
+  // publishes low-level commands that mavros understands
+  rclcpp::Publisher<mavros_msgs::msg::PositionTarget>::SharedPtr target_pub_;
+};
+
+} // namespace kestrel_control
+#endif // KESTREL_CONTROL__ARDUPILOT_TRANSLATOR_NODE_HPP_
