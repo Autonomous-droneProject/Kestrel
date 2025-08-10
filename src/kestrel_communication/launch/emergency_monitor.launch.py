3a5a77e03444466e27f155fc73ed79e520d3cb73
BigManRaffa <animedudebro@gmail.com>
2025-08-09 20:49:48 -0400

    launch 3

---
+++ src/kestrel_communication/launch/emergency_monitor.launch.py
@@ -0,0 +1,27 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    kestrel_communication_share = get_package_share_directory('kestrel_communication')
+
+    emergency_handler_node = Node(
+        package='kestrel_communication',
+        executable='emergency_handler',
+        name='emergency_handler',
+        parameters=[os.path.join(kestrel_communication_share, 'config', 'emergency_triggers.yaml')]
+    )
+
+    heartbeat_monitor_node = Node(
+        package='kestrel_communication',
+        executable='heartbeat_monitor',
+        name='heartbeat_monitor',
+        parameters=[os.path.join(kestrel_communication_share, 'config', 'comms_params.yaml')]
+    )
+
+    return LaunchDescription([
+        emergency_handler_node,
+        heartbeat_monitor_node
+    ])
