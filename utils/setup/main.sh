fd6deb477b20d87aa979faedf7f28bd272b0b849
Syndric <65517819+Syndric@users.noreply.github.com>
2025-08-13 18:52:35 -0400

    add: base sh

---
+++ utils/setup/main.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script automates the install of literally everything (hopefully) on a fresh
+# Ubuntu 24.04 (noble) installation inside WSL.
+# Assumes fresh install and minimal changes to the system.
+
+# To run this file, navigate to where this file is an run "main.sh"
